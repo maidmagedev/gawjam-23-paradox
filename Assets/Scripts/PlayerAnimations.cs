7bb30a4f505f4be212f53b7f02be910251214956
maidmage <46210759+maidmagedev@users.noreply.github.com>
2023-11-09 05:00:20 -0500

    Update PlayerAnimations.cs

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -20,6 +20,10 @@ public class PlayerAnimations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (perspecMan.currentPerspective == PerspectiveManager.PerspectiveMode.sidescroller) {
+
+        } else {
+            
+        }
     }
 }
