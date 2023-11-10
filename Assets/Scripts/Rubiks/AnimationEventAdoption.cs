6faccb4e5214f7fadef3f497420db4b47746a4a1
maidmage <46210759+maidmagedev@users.noreply.github.com>
2023-11-09 22:43:53 -0500

    Structuring changes, +unfinished code

---
+++ Assets/Scripts/Rubiks/AnimationEventAdoption.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventAdoption : MonoBehaviour
+{
+    List<GameObject> Rows = new List<GameObject>();
+
+    public void Adopt() {
+        Debug.Log("Adopt.");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
